557f659ee0f87db39f26d1f701fef3e37aec6444
Rohan07Singh <80488308+Rohan07Singh@users.noreply.github.com>
2021-04-25 22:41:49 +0530

    Add files via upload

---
+++ newline.py
@@ -0,0 +1,14 @@
+print("Python")
+print("is easy to learn.")
+
+# new line
+print()
+
+# print both the statements on a single line
+print("Python", end=" ")
+print("is easy to learn.")
+print("""Python
+is
+easy 
+to
+learn""")
